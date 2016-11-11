c9eeea3fa978036a2ad88925684b62192c481db0
pfischbacher <pfischbacher@users.noreply.github.com>
2016-11-12 08:42:32 +0900

    Create GenomicRangeQuery.js

---
+++ GenomicRangeQuery.js
@@ -0,0 +1,33 @@
+function solution(S, P, Q) {
+    var results = [], i = 0;
+    S = S.replace(/A/g, 1);
+    S = S.replace(/C/g, 2);
+    S = S.replace(/G/g, 3);
+    S = S.replace(/T/g, 4);
+    
+    var cum_impact_factors = [0,0,0,0];
+    var cum_impact_factors_arr = [];
+    cum_impact_factors_arr.push(cum_impact_factors);
+    
+    for(i = 0; i < S.length; i++) {
+        cum_impact_factors = cum_impact_factors.concat();
+        cum_impact_factors[S[i]-1]++;
+        cum_impact_factors_arr.push(cum_impact_factors);
+    }
+    
+    for(i = 0; i < P.length; i++) {
+        var min = 0;
+        result = 0;
+        for (j = 0; j < 4; j++) {
+            min = cum_impact_factors_arr[Q[i]+1][j] - cum_impact_factors_arr[P[i]][j];
+            if (min > 0) {
+                result = j + 1;
+                break;
+            }
+        }
+            
+        results.push(result);
+    }
+    
+    return results;
+}
