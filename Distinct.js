7e274d7a72f762a625a1c833a74bbe0347e67f70
pfischbacher <pfischbacher@users.noreply.github.com>
2016-11-15 20:13:21 +0900

    Create Distinct.js

---
+++ Distinct.js
@@ -0,0 +1,10 @@
+function solution(A) {
+    return uniq(A).length;
+}
+
+function uniq(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
