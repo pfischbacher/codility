ae1131a3af6ea620ad8a7d7b3ab5c93cd1b31a70
pfischbacher <pfischbacher@users.noreply.github.com>
2016-11-24 19:25:19 +0900

    Create sample_test.js

---
+++ sample_test.js
@@ -0,0 +1,35 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+/*function solution(A) {
+    // write your code in JavaScript (Node.js 6.4.0)
+    //Brute force method.
+    var sum_i = 0, sum_j = 0;
+    for (var i = 0; i < A.length; i++) {
+        sum_i += A[i];
+        sum_j = 0;
+        for (var j = i+2; j < A.length; j++) {
+            sum_j += A[j];
+        }
+        if (sum_i === sum_j)
+            return i+1;
+    }
+    
+    return -1;
+}*/
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 6.4.0)
+    var total = 0, sum_i = 0, sum_j = 0;
+    for (var i = 0; i < A.length; i++) {
+        total += A[i];
+    }
+    for (var j = 0; j < A.length - 1; j++) {
+        sum_i +=A[j];
+        sum_j = total - sum_i - A[j+1];
+        if (sum_i === sum_j)
+            return j+1;
+    }
+    
+    return -1;
+}
